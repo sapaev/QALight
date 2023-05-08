f329d2cf397f621fb71208cdfdb3c7e20e1e0291
qasap <qa,sapaev@gmail.com>
2023-05-08 18:20:29 +0300

    Дипломный проект

--- src/test/java/RegressionTest/RegressionTest.java
+++ src/test/java/RegressionTest/RegressionTest.java
@@ -3,9 +3,12 @@ package RegressionTest;
 import CheckOfItemCounterInItemHistoryTest.CheckOfItemCounterInItemHistoryTest;
 import EditProfileTest.EditProfileTest;
 import FindItemWithFilterTest.FindItemWithFilterTest;
+import ItemPageTest.ItemPageTest;
 import LogOutTest.LogOutTest;
+import LoginTest.LoginTest;
 import RegistrationTest.RegistrationErrorText;
 import RegistrationTest.RegistrationTest;
+import SearchItemWithSearchField.SearchItemWithSearchFieldTest;
 import checkLocalizationLink.LocalizationLinkTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -16,8 +19,11 @@ import org.junit.runners.Suite;
         CheckOfItemCounterInItemHistoryTest.class,
         EditProfileTest.class,
         FindItemWithFilterTest.class,
+        ItemPageTest.class,
+        LoginTest.class,
         LogOutTest.class,
         RegistrationErrorText.class,
+        SearchItemWithSearchFieldTest.class,
 })
 public class RegressionTest {
 }
